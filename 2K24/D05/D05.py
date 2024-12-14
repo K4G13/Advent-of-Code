d18ce72e70d24b0276219fb291acd16e78ca0d3b
“k4g13” <polcyn.piotrek@gmail.com>
2024-12-14 21:13:23 +0100

    D05 2/2

--- 2K24/D05/D05.py
+++ 2K24/D05/D05.py
@@ -29,3 +29,29 @@ count_sum = 0
 for u in updates:
     if check_update(u): count_sum += int(u[len(u)//2])
 print(count_sum)
+
+# PART 2
+
+incorrect = []
+for u in updates: 
+    if not check_update(u): incorrect.append(u)
+
+def correct_order(update):
+    d = {}
+    for i in range(0,len(u)):
+        restricted = list_restricted(u[i])
+        count = 0
+        for el in restricted:
+            if el in u: count += 1
+        d[u[i]] = count        
+    d = dict(sorted(d.items(), key=lambda item: item[1]))
+    res = []
+    for k,v in d.items():
+        res.append(k)
+    return res
+
+count_sum = 0
+for u in incorrect:
+    correct = correct_order(u)
+    count_sum += int(correct[len(correct)//2])
+print(count_sum)
