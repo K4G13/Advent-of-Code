a459c967bc7895b8aef1569aecd270d64726c3f7
k4g13 <polcyn.piotrek@gmail.com>
2025-12-08 03:08:34 +0100

    D06

--- 2k25/D06/D06.py
+++ 2k25/D06/D06.py
@@ -17,3 +17,30 @@ for i,op in enumerate(operations):
     grandTotal += result
 
 print(grandTotal)
+
+# PART 02
+import numpy as np 
+rawRows = open("input","r").read().split("\n")
+operators = rawRows[-1].split()
+M = np.array(list(map(list,rawRows[:-1])))
+M = np.rot90(M)
+
+grandTotal = 0
+partialTotal = 0
+opIdx = len(operators) - 1
+for row in M:
+
+    s = "".join(row).strip(" ")
+    if s == "": 
+        opIdx -= 1
+        grandTotal += partialTotal
+        partialTotal = 1 if operators[opIdx] == "*" else 0
+        continue
+
+    number = int(s)
+
+    if operators[opIdx] == "*": partialTotal *= number
+    if operators[opIdx] == "+": partialTotal += number
+grandTotal += partialTotal
+
+print(grandTotal)
